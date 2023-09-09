f4c6e3d586b21def4af6441a0e263a356f260aeb
mathcamargo <matheusoabes@gmail.com>
2023-09-09 16:22:12 -0300

    adicionando opção de visualizar dados cadastrados

--- Challenge_Sprint_3/main.py
+++ Challenge_Sprint_3/main.py
@@ -2,14 +2,18 @@ from lib.subalgoritmos import *
 import os
 from time import sleep
 
-funcionalidades = ['Cadastrar dados pessoais', 'Cadastrar dados da bike', 'Ver/Selecionar plano', 'Perguntas frequentes',
-         'Fechar o programa']
+funcionalidades = ['Ver/Cadastrar dados pessoais', 'Ver/Cadastrar dados da bike', 'Ver/Selecionar plano', 'Fechar o programa']
 dados = {'Dados Pessoais': {'Nome': "", 'Email': "", 'Telefone': "", 'CPF': ""},
         'Dados Bike': {'Marca': "", 'Modelo': "", 'Chassi': "", 'Valor': 0.0}}
 acessorio = {'Tipo do acessório': "", 'Marca do acessório': "",
             'Modelo do acessório': "", 'Preço do acessório': 0.0}
-opc_menu = 1
-plano = ""
+quant_acessorio = 0
+opc_menu = 0
+opc_plano = 0
+plano_selecionado = ""
+alterar_plano = ""
+d_pessoais_cadastrados = False
+d_bike_cadastrados = False
 
 os.system('cls')
 print(f"""
@@ -20,38 +24,49 @@ print(f"""
 {linha(61)}
 """)
 sleep(3)
-while opc_menu != 5:
+while opc_menu != 4:
     menu('MENU PRINCIPAL', funcionalidades)
     opc_menu = int(tratar_erro_num("Escolha uma opção: "))
     match opc_menu:
         case 1:
-            dados['Dados Pessoais'] = cadastro_geral('DADOS PESSOAIS', dados['Dados Pessoais'])
-            dados['Dados Pessoais'] = confirmar_dados('DADOS PESSOAIS', dados['Dados Pessoais'])
-            cabecalho("Cadastro de dados pessoais finalizado!!!")
+            if not d_pessoais_cadastrados:
+                dados['Dados Pessoais'] = cadastro_geral('DADOS PESSOAIS', dados['Dados Pessoais'])
+                dados['Dados Pessoais'] = confirmar_dados('DADOS PESSOAIS', dados['Dados Pessoais'])
+                d_pessoais_cadastrados = True
+                cabecalho("Cadastro de dados pessoais finalizado!!!")
+            else:
+                dados['Dados Pessoais'] = confirmar_dados('DADOS PESSOAIS', dados['Dados Pessoais'])
         case 2:
-            dados['Dados Bike'] = cadastro_geral('DADOS BIKE', dados['Dados Bike'])
-            dados['Dados Bike'] = confirmar_dados('DADOS BIKE', dados['Dados Bike'])
-            while True:
-                possui_acessorio = input("Sua bike possui algum acessório? [S/N]: ")
-                if possui_acessorio.upper() == 'S' or possui_acessorio.upper() == 'N':
-                    break
-                else:
-                    exibir_invalido()
-            if possui_acessorio.upper() == 'S':
-                quant_acessorio = int(tratar_erro_num("Quantos?: "))
-                for i in range(quant_acessorio):
-                    dados[f'Acessório {i + 1}'] = acessorio
-                    dados[f'Acessório {i + 1}'] = cadastro_geral(f'ACESSÓRIO {i + 1}', dados[f'Acessório {i + 1}'])
-                    dados[f'Acessório {i + 1}'] = confirmar_dados(f'ACESSÓRIO {i + 1}', dados[f'Acessório {i + 1}'])
-            if possui_acessorio.upper() == 'N':
-                os.system("cls")
-            cabecalho("Cadastro de dados da bike finalizado!!!")
+            if not d_bike_cadastrados:
+                dados['Dados Bike'] = cadastro_geral('DADOS BIKE', dados['Dados Bike'])
+                dados['Dados Bike'] = confirmar_dados('DADOS BIKE', dados['Dados Bike'])
+                while True:
+                    possui_acessorio = input("Sua bike possui algum acessório? [S/N]: ")
+                    if possui_acessorio.upper() == 'S' or possui_acessorio.upper() == 'N':
+                        break
+                    else:
+                        exibir_invalido()
+                if possui_acessorio.upper() == 'S':
+                    quant_acessorio = int(tratar_erro_num("Quantos?: "))
+                    for i in range(quant_acessorio):
+                        dados[f'Acessório {i + 1}'] = acessorio
+                        dados[f'Acessório {i + 1}'] = cadastro_geral(f'ACESSÓRIO {i + 1}', dados[f'Acessório {i + 1}'])
+                        dados[f'Acessório {i + 1}'] = confirmar_dados(f'ACESSÓRIO {i + 1}', dados[f'Acessório {i + 1}'])
+                if possui_acessorio.upper() == 'N':
+                    os.system("cls")
+                d_bike_cadastrados = True    
+                cabecalho("Cadastro de dados da bike finalizado!!!")
+            else:
+                dados['Dados Bike'] = confirmar_dados('DADOS BIKE', dados['Dados Bike'])
+                if quant_acessorio != 0:
+                    for i in range(quant_acessorio):
+                        dados[f'Acessório {i + 1}'] = confirmar_dados(f'ACESSÓRIO {i + 1}', dados[f'Acessório {i + 1}'])
         case 3:
-            opc_plano = 1
-            os.system('cls')
-            while opc_plano != 0 and plano == "":
-                cabecalho("SELECIONE SEU PLANO")
-                print(f"""1 --> Pedal essencial: O plano para você 
+            while True:
+                os.system('cls')
+                if plano_selecionado == "":
+                    cabecalho("SELECIONE SEU PLANO")
+                    print(f"""1 --> Pedal essencial: O plano para você 
 experimentar um dos serviços essenciais 
 oferecidos, de acordo com as suas necessidades.
 --------------------------------------------------
@@ -67,18 +82,21 @@ nível.
 --------------------------------------------------
 5 --> Voltar.
 {linha(50)}""")
-                opc_plano = int(tratar_erro_num("Escolha uma opção: "))
-                match opc_plano:
-                    case 1:
-                        plano = 'Pedal essencial'
-                    case 2:
-                        plano = 'Pedal leve'
-                    case 3:
-                        plano = 'Pedal elite'
-                    case 4:
-                        os.system('cls')
-                        cabecalho("INFORMAÇÕES SOBRE PLANOS")
-                        print(f"""\033[1m--> Pedal Essencial:\033[0m plano gratuito que oferece 
+                    opc_plano = int(tratar_erro_num("Escolha uma opção: "))
+                    match opc_plano:
+                        case 1:
+                            plano_selecionado = 'Pedal essencial'
+                            break
+                        case 2:
+                            plano_selecionado = 'Pedal leve'
+                            break
+                        case 3:
+                            plano_selecionado = 'Pedal elite'
+                            break
+                        case 4:
+                            os.system('cls')
+                            cabecalho("INFORMAÇÕES SOBRE PLANOS")
+                            print(f"""\033[1m--> Pedal Essencial:\033[0m plano gratuito que oferece 
 reparo e/ou troca de câmaras de ar, correntes, 
 coroas, manetes de freios, além de 
 lubrificação de correntes.
@@ -93,19 +111,29 @@ Essencial oferece, com um benefício a mais:
 transporte do segurado e sua bike em caso de 
 quebra ou acidente, com limite de \033[1m150 km\033[0m.
 {linha(50)}""")
-                        enter = input("Pressione ENTER para voltar")
-                    case 5:
-                        cabecalho('VOLTANDO...')
-                        sleep(1.5)
-                    case _:
-                        exibir_invalido()
-                if opc_plano != 4:
-                    os.system('cls')
-                if plano != "":
-                    cabecalho(f"{plano} selecionado!!!")
+                            input("Pressione ENTER para voltar")
+                        case 5:
+                            cabecalho('VOLTANDO...')
+                            sleep(1.5)
+                            break
+                        case _:
+                            exibir_invalido()
+                else:
+                    cabecalho(f"{plano_selecionado} selecionado.")
+                    while True:
+                        alterar_plano = input("Deseja alterar seu plano? [S/N]: ")
+                        if alterar_plano.upper() == 'S' or alterar_plano.upper() == 'N':
+                            break
+                        else:
+                            exibir_invalido()
+                    if alterar_plano.upper() == 'S':
+                        plano_selecionado = ""
+                    else:
+                        break
+            os.system('cls')
+            if plano_selecionado != "":
+                        cabecalho(f"{plano_selecionado} selecionado!!!")
         case 4:
-            print("")
-        case 5:
             cabecalho("Finalizando programa...")
             cabecalho("Desenvolvido por: CycleX")
         case _:
